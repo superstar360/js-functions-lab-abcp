df9f163ff8b38b4aaa32e3b10aeae1327c3164fc
Abel Sobmenso Dadbutiso <abelsobmenso@gmail.com>
2018-01-09 20:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!"
 }
 
-function holidayCountdown(holiday, days)
+function holidayCountdown(holiday, days) {
+  return "It\'s ${days} days until ${holiday}!"
+}
